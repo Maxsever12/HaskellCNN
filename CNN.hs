2abf31142e0a2c36db2453ce08c774eb334e1f3c
Tyson Schneider <tyson@Tysons.home>
2026-03-30 15:55:16 +1000

    MaxPooling function added. Pools the output of the channel.

--- CNN.hs
+++ CNN.hs
@@ -45,9 +45,31 @@ main = do
     print "Convolutional Layer Filters:"
     print $ convolution convLayer
     print "Convolution Applied:"
-    print $ forward (convolution convLayer) [generateRegions 3 channel]
+    let c = forward (convolution convLayer) [generateRegions filterSizeGlobal channel]
+    print c
+    print "Max Pooling Applied:"
+    print $ map maxPool2 c
   
     
+
+maxPool2 :: Channel -> Channel
+maxPool2 channel = Channel
+  { height = height channel `div` 2
+  , width = width channel `div` 2
+  , pixels = poolPixels (pixels channel)
+  }
+
+poolPixels :: [[Float]] -> [[Float]]
+poolPixels [] = []
+poolPixels (row1:row2:rows) = poolRows row1 row2 : poolPixels rows
+poolPixels _ = [] -- Handle odd number of rows
+
+poolRows :: [Float] -> [Float] -> [Float]
+poolRows [] [] = []
+poolRows (x1:x2:xs) (y1:y2:ys) = max (max x1 x2) (max y1 y2) : poolRows xs ys
+poolRows _ _ = [] -- Handle odd number of columns
+
+
 
 
 forward :: [[[[Float]]]] -> [[[Channel]]] -> [Channel]
